683be6a745715c1628cf1cbace06ffe827612466
salvador <salvadorjnt@gmail.com>
2024-05-21 18:41:23 -0600

    fetch and render data from public API

--- src/breakingbad/breakingbad-app.js
+++ src/breakingbad/breakingbad-app.js
@@ -1,7 +1,31 @@
 /**
+ * @returns {Object} quote information
+ */
+const fetchQuote = async() => {
+    const response = await fetch('https://api.breakingbadquotes.xyz/v1/quotes');
+    const data = await response.json();
+    console.log(data[0]);
+    return data[0];
+}
+
+
+/**
  * 
  * @param {HTMLDivElement} element 
  */
-export const BreakingBadApp = (element) => {
+export const BreakingBadApp = async (element) => {
     element.innerHTML = 'Loading...';
+    const quoteLabel = document.createElement('blockquote');
+    const authorLabel = document.createElement('h3');
+    const nextQuoteButton = document.createElement('button');
+    nextQuoteButton.innerHTML = 'Next Quote';
+
+    const renderQuote = (data) => {
+        quoteLabel.innerHTML = data.quote;
+        authorLabel.innerHTML = data.author;
+        element.replaceChildren(quoteLabel, authorLabel, nextQuoteButton);
+    }
+
+    fetchQuote()
+    .then(renderQuote);
 }
